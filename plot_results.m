9eaa819746cd9d227fc2dede95fad3a359efc7c8
heloisa <heloisa.barbosa@wr.sc.usp.br>
2020-04-15 18:00:02 -0300

    Update plot_results.m

--- plot_results.m
+++ plot_results.m
@@ -1,16 +1,17 @@
 
-file = fopen('.../castaway_probem-repo-master/world/data.csv');
+file = fopen('../world/data.csv');
 tline = fgetl(file);
 figure();
 
 while ischar(tline)
     C2 = textscan(tline,'%f32 %f32 %f32 %f32 %f32','Delimiter',';');
-    plot(C2{2}, C2{3},'bo', C2{4}, C2{5},'ro');
+    clf
+	plot(C2{2}, C2{3},'bo', C2{4}, C2{5},'ro');
+	grid on
     axis([-1.2 1.2 -1.2 1.2]);
-    tline = fgetl(files);
+    tline = fgetl(file);
     pause(0.00000001);
     hold on
-    grid on;
 end
 fclose(file);
 hold off
