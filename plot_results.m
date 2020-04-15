d4e75e624037097086b87344ac8bbf14e1f20b81
heloisa <heloisa.barbosa@wr.sc.usp.br>
2020-04-15 18:21:51 -0300

    Update plot_results.m

--- plot_results.m
+++ plot_results.m
@@ -6,9 +6,11 @@ figure();
 while ischar(tline)
     C2 = textscan(tline,'%f32 %f32 %f32 %f32 %f32','Delimiter',';');
     clf
-	plot(C2{2}, C2{3},'bo', C2{4}, C2{5},'ro');
-	grid on
+    plot(C2{2}, C2{3},'bo', C2{4}, C2{5},'ro');
+    grid on
+    viscircles([0,0], 1.0,'LineStyle',':', 'Color', 'k');
     axis([-1.2 1.2 -1.2 1.2]);
+    axis square;
     tline = fgetl(file);
     pause(0.00000001);
     hold on
