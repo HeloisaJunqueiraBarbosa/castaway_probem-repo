ac7cc5fed0dbde03e2c420facd6ca7aee92f25ab
HeloisaJunqueiraBarbosa <48660431+HeloisaJunqueiraBarbosa@users.noreply.github.com>
2020-04-16 10:07:17 -0300

    Update main.cc

--- human/main.cc
+++ human/main.cc
@@ -15,7 +15,7 @@ Angle angular_distance(const Position& human, const Position& shark) {
     return(distance);
 }
 
-//comṕare the time to castaway runs in a linear rajectory anf the time to shark reaches him
+//compare the time to the castaway runs in a linear trajectory and the time that it takesto the to shark reach him
 bool timeToBorder(const Position& human, const Position& shark){
     float dist_border, tempoHumano, tempoTubarao, velocidade_tubarao, velocidade_humano;
     velocidade_humano = 1.0;
@@ -57,12 +57,12 @@ Velocity goTo(const Position& human, const Position& destino) {
     return(velocidade);
 }
 
-// velocity to reaches the radius to run in circle
+// velocity to reach the radius to run in circle
 Velocity goTo_with_PID(const Position& human, float pdestino, float angular_distance_lastError[3], float* last_aux){
     float aux, dist, dist_destino, distance;
     float dt, Kp, Kd, Td, T0, angular_distance_aux_U0, angular_distance_aux_Q0, angular_distance_aux_Q1, angular_distance_aux_Q2, angular_distance_past;
     dt = 0.1;                                   // interval time to execute a program is 100ms
-    Kp = 0.5;                                   //proporcional gain
+    Kp = 0.5;                                   //proportional gain
     Kd = 0.01;                                  //derivative gain
     //Ki = 0.0;                                 //integral gain
     T0 = dt;                               
@@ -101,12 +101,12 @@ Velocity goTo_with_PID(const Position& human, float pdestino, float angular_dist
     return(velocidade);
 }
 
-// circular velocity
+// angular velocity
 Velocity moveAtRadius(const Position& human, float radius){
     float vx=0.0, vy=0.0, teta = 0.0;
     
     Angle angleHuman(true, atan2(human.y(), human.x()));
-    teta = (((1/radius)*(0.1/2))+ angleHuman.value());    //delta_teta = (w*t+teta)=(((v_c)/(t/2))+teta) using a t/2 has a small inteval
+    teta = (((1/radius)*(0.1/2))+ angleHuman.value());    //delta_teta = (w*t+teta)=(((v_c)/(t/2))+teta) using a t/2 gives a small interval
     vx = -1*sin(teta);
     vy = 1*cos(teta);
     Velocity velocity(true, vx, vy);
@@ -139,12 +139,12 @@ int main(int argc, char** argv) {
         dist= (sqrt(pow(human.x(),2)+pow(human.y(),2)));
         Angle angular_dist = angular_distance(human, shark);
 
-        if(timeToBorder(human, shark)==true){           								 //verify if castaway can run linearly to the border before the castaway reaches him
-            Velocity velocity_linear = goTo(human, pontoDestino(human.x(), human.y()));  //run linearly to reaches the border
+        if(timeToBorder(human, shark)==true){           								 //verify if the castaway can run linearly to the border before the shark reaches him
+            Velocity velocity_linear = goTo(human, pontoDestino(human.x(), human.y()));                                  //run linearly to reaches the border
             humanX = velocity_linear.x();            
             humanY = velocity_linear.y();
         }
-        else                                            								//run around the center in a radius between 0.215<radius<0.225 (the smaller the radius the fewer turns he needs to give)
+        else                                            								//run around the center in a radius between 0.215<radius<0.225 (the smaller the radius less turns he needs to run)
         { 
             if(align == true ){
                 if(dist < 0.250){                       								// run in a circular trajectory with a fixed radius
@@ -157,7 +157,7 @@ int main(int argc, char** argv) {
                     align = false;
                 }
             }
-            else{                                       								//try reache the radius and waiting for the shark to align
+            else{                                       								//castaway tries to reach the radius and he is waiting for the shark to align
                 if((dist < 0.225) && (dist > 0.215)){     
                     if(((angular_dist.value() < 0.001) && (angular_dist.value() > -0.001))|| (angular_dist.value() > 6.282) || (angular_dist.value() < -6.282) ){
                         humanX = 0.0;
